c6b9c389129fceadc2bfbdde32a96f7f73022f29
unknown <paul@BMCV000348.thebmc.local>
2012-05-21 11:10:34 +0100

    adding in memory manipulation

--- IndexExploration/ListRecipientsByDate.cs
+++ IndexExploration/ListRecipientsByDate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using NUnit.Framework;
+using Raven.Client.Linq;
 
 namespace IndexExploration
 {
@@ -19,9 +20,81 @@ namespace IndexExploration
 
                 var unrealisticDate = new DateTime(3014, 4, 1);
                 var actual = session.Query<PersonMailing, Indexes.RecipientsByDate>()
-                                        .ToList();
+                                        .AsProjection<PersonMailing>().ToList();
                 Assert.True(!actual.Any());
             }
+        }
+
+        [Test]
+        public void ShouldGetEmptyListWhenNoMailingIsPresentInMemoryMethod()
+        {
+            using (var session = _store.OpenSession())
+            {
+                session.Store(_first);
+                session.Store(_second);
+                session.SaveChanges();
+                session.ClearStaleIndexes();
+
+                var unrealisticDate = new DateTime(3014, 4, 1);
+
+                var candidates = from person in session.Query<Person>().ToList()
+                                  from body in person.Bodies
+                                  where body.MailingDate.Date == unrealisticDate.Date
+                                  select
+                                      new PersonMailing
+                                          {
+                                              MailingBody = body.BodyText,
+                                              MailingDate = body.MailingDate,
+                                              MembershipNumber = person.MembershipNumber
+                                          };
+
+                Assert.True(!candidates.Any());
+            }
+        }
+
+        [Test]
+        public void ShouldGetTwoInListWithExpectedValueInMemoryMethod()
+        {
+            using (var session = _store.OpenSession())
+            {
+                session.Store(_first);
+                session.Store(_second);
+                session.SaveChanges();
+                session.ClearStaleIndexes();
+
+                var unrealisticDate = new DateTime(3014, 4, 1);
+
+                var candidates = from person in session.Query<Person>().ToList()
+                                 from body in person.Bodies
+                                 where body.MailingDate.Date == TargetDate.Date
+                                 select
+                                     new PersonMailing
+                                     {
+                                         MailingBody = body.BodyText,
+                                         MailingDate = body.MailingDate,
+                                         MembershipNumber = person.MembershipNumber
+                                     };
+
+                Assert.AreEqual(2,candidates.Count());
+            }
+        }
+
+        [Test]
+        public void ShouldGetTwoInListWithExpectedValue()
+        {
+            using (var session = _store.OpenSession())
+            {
+                session.Store(_first);
+                session.Store(_second);
+                session.SaveChanges();
+                session.ClearStaleIndexes();
+
+                var actual = session.Query<PersonMailing, Indexes.RecipientsByDate>()
+                                    .Where(p => p.MailingDate.Date == TargetDate.Date);
+                Assert.IsNotNull(actual);
+                Assert.AreEqual(2, actual.Count());
+            }
+
         }
     }
 }
